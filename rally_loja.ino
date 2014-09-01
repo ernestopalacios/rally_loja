44c759bb689e51aced51145f30f9c469818b5285
ernestopalacios <mecatronica.mid@gmail.com>
2014-09-01 17:41:10 -0500

    Dibuja cada segundo, per hay momentos que vuelve a dibujar el mismo segundo, además tiene un poco de parpadeo al cambiar de segundo.

--- rally_loja.ino
+++ rally_loja.ino
@@ -78,6 +78,8 @@ void setup(void)
    //initialize TimerOne's interrupt/CPU usage used to scan and refresh the display
    Timer1.initialize( 5250 );            //period in microseconds to call ScanDMD. Anything longer than 5000 (5ms) and you can see flicker.
    Timer1.attachInterrupt( ScanDMD );   //attach the Timer1 interrupt to ScanDMD which goes to dmd.scanDisplayBySPI()
+   dmd.selectFont(Arial_Black_16);     // Mover al void_setup()
+
 
    //clear/init the DMD pixels held in RAM
    dmd.clearScreen( true );   //true is normal (all pixels off), false is negative (all pixels on)
@@ -99,42 +101,11 @@ void setup(void)
    
 void loop(void)
 {
-   boton=digitalRead(PIN_SWICTH_LARGADA);    // Lee el pin de entrada para el Switch
+   boton=digitalRead( PIN_SWICTH_LARGADA );    // Lee el pin de entrada para el Switch
    hora=hour();
    minuto=minute();
    segundo=second();
    
-   
-  
- /////////// CONVERSIONES DE INT A CONST CHAR ////////
-   str=String(hour());
-   str.toCharArray(hora_char,3);
-   str=String(minute());
-   str.toCharArray(minuto_char,3);
-   str=String(second());
-   str.toCharArray(segundo_char,3);
-////////////////////////////////////////////////////
-   
-   if (boton==1){
-     Reloj_Largada(); 
-   }
-   else{
-     Reloj_Normal();
-     bandera_limpiar=0;
-   }
-   
-     
-   if ((segundo==1 || segundo==21 || segundo==31 || segundo==41 || segundo==51 || segundo==10) && bandera2==0){
-     dmd.clearScreen(true);
-     bandera2=1;
-   }
-   else if(segundo==11 && bandera2==1){
-      dmd.clearScreen(true);
-      bandera2=0; 
-   }
-   else if(segundo!=1 && segundo!=11 && segundo!=21 && segundo!=31 && segundo!=41 && segundo!=51 && segundo!=10 && bandera2==1){
-     bandera2=0; 
-   }
    
    if( Serial.available()>0 ){       
       val[i]=Serial.read();  
@@ -169,38 +140,82 @@ void loop(void)
    
 void pulso(void){
 
+   // El codigo dentro del IF se ejecuta cada segundo
    if( digitalRead(PIN_PULSO_1HZ) )
    {
        
-      Serial.print( hora, DEC);
-      Serial.print( ":");
-      Serial.print( minuto, DEC);
-      Serial.print( ":");
-      Serial.print( segundo, DEC);
-      Serial.print("\n");
+      /* Modo de Prueba Serial: [OK] /
+         Serial.print( hora, DEC);
+         Serial.print( ":");
+         Serial.print( minuto, DEC);
+         Serial.print( ":");
+         Serial.print( segundo, DEC);
+         Serial.print("\n");
+      // */
+
+      if ( boton == 1){
+         Reloj_Largada(); 
+      }
+      else{
+         Reloj_Normal();
+         bandera_limpiar=0;
+      }
+
+
+      if ((segundo==1 || segundo==21 || segundo==31 || segundo==41 || segundo==51 || segundo==10) && bandera2==0){
+         dmd.clearScreen(true);
+         bandera2=1;
+      }
+      else if(segundo==11 && bandera2==1){
+         dmd.clearScreen(true);
+         bandera2=0; 
+      }
+      else if(segundo!=1 && segundo!=11 && segundo!=21 && segundo!=31 && segundo!=41 && segundo!=51 && segundo!=10 && bandera2==1){
+         bandera2=0; 
+      }
+
+   }
+
+   // El codigo dentro del Else se ejecuta a la mitad del segundo a dibujar
+   else{    
+      // PREPARA ANTES DE DIBUJAR
+      // 
+      // 
+        
+      /////////// CONVERSIONES DE INT A CONST CHAR ////////
+         str = String( hour() );
+         str.toCharArray( hora_char, 3 );
+         
+         str = String( minute() );
+         str.toCharArray( minuto_char, 3 );
+         
+         str=String( second() );
+         str.toCharArray( segundo_char, 3 );
+      ////////////////////////////////////////////////////
+      
    }
 }   
 
 
 void Reloj_Normal (void){
-   dmd.selectFont(Arial_Black_16);
-   if (hora<10){
-     dmd.drawString(  0, 1 ,"0", 1, GRAPHICS_NORMAL );
-     dmd.drawString(  9, 1 , hora_char, 2, GRAPHICS_NORMAL );
+   
+   if ( hora < 10){
+      dmd.drawString(  0, 1 ,"0", 1, GRAPHICS_NORMAL );
+      dmd.drawString(  9, 1 , hora_char, 2, GRAPHICS_NORMAL );
    }
    else{
-       dmd.drawString(  0, 1 , hora_char, 2, GRAPHICS_NORMAL );
+      dmd.drawString(  0, 1 , hora_char, 2, GRAPHICS_NORMAL );
    }
    
    dmd.drawBox( 19,9 , 20, 10, GRAPHICS_NORMAL );
    dmd.drawBox( 19,5 , 20, 6, GRAPHICS_NORMAL );
    
-   if (minuto<10){
-     dmd.drawString( 22, 1 ,"0", 1, GRAPHICS_NORMAL );
-     dmd.drawString( 31, 1 ,minuto_char, 2, GRAPHICS_NORMAL );          
+   if ( minuto < 10){
+      dmd.drawString( 22, 1 ,"0", 1, GRAPHICS_NORMAL );
+      dmd.drawString( 31, 1 ,minuto_char, 2, GRAPHICS_NORMAL );          
    }
    else{
-       dmd.drawString( 22, 1 , minuto_char, 2, GRAPHICS_NORMAL );
+      dmd.drawString( 22, 1 , minuto_char, 2, GRAPHICS_NORMAL );
    }
    
    
@@ -208,27 +223,29 @@ void Reloj_Normal (void){
    dmd.drawBox( 42,5 , 43, 6, GRAPHICS_NORMAL );
    
   
-   if(segundo<10){
-     dmd.drawString(  45, 1 ,"0", 1, GRAPHICS_NORMAL );
-     dmd.drawString(  54, 1 ,segundo_char, 2, GRAPHICS_NORMAL );
+   if( segundo < 10){
+      dmd.drawString(  45, 1 ,"0", 1, GRAPHICS_NORMAL );
+      dmd.drawString(  54, 1 ,segundo_char, 2, GRAPHICS_NORMAL );
    }
    else{
 
-       dmd.drawString(  45, 1 , segundo_char, 2, GRAPHICS_NORMAL );
+      dmd.drawString(  45, 1 , segundo_char, 2, GRAPHICS_NORMAL );
    }
 }
 
 
 void Reloj_Largada(void){
-   segundo_inv=60-segundo;
-   str=String(segundo_inv);
-   str.toCharArray(segundo_reg,3);
    
-   if(segundo<50 && segundo>0){
+   segundo_inv = 60 - segundo;
+   str=String( segundo_inv );
+   str.toCharArray( segundo_reg, 3 );
+   
+   if( segundo < 50 && segundo > 0){
      Reloj_Normal();
    }
-   else if(segundo<=55 && segundo>0){
-    if (bandera_limpiar==0){
+   
+   else if( segundo <= 55 && segundo > 0){
+    if ( bandera_limpiar == 0 ){
       dmd.clearScreen( true );
       bandera_limpiar=1;
     } 
@@ -255,75 +272,84 @@ void Reloj_Largada(void){
     
     dmd.drawString(  47, 1 , segundo_reg, 2, GRAPHICS_NORMAL );
    }
-   else if (segundo==56){
-    if (bandera_limpiar==0){
-      dmd.clearScreen( true );
-      bandera_limpiar=1;
-    } 
-    dmd.drawCircle( 4, 8, 3, GRAPHICS_INVERSE );
-    dmd.drawFilledBox( 3, 6, 5,10, GRAPHICS_INVERSE );
-    dmd.drawFilledBox( 2, 7, 6,9, GRAPHICS_INVERSE );
-    
-    dmd.drawCircle( 13, 8, 3, GRAPHICS_NORMAL );
-    dmd.drawCircle( 22, 8, 3, GRAPHICS_NORMAL );
-    dmd.drawCircle( 31, 8, 3, GRAPHICS_NORMAL );
-    dmd.drawCircle( 40, 8, 3, GRAPHICS_NORMAL );
-    
-    dmd.drawString(  47, 1 , segundo_reg, 2, GRAPHICS_NORMAL );
+
+   else if ( segundo == 56){
+      
+      if ( bandera_limpiar == 0 ){
+         dmd.clearScreen( true );
+         bandera_limpiar = 1;
+      } 
+
+      dmd.drawCircle( 4, 8, 3, GRAPHICS_INVERSE );
+      dmd.drawFilledBox( 3, 6, 5,10, GRAPHICS_INVERSE );
+      dmd.drawFilledBox( 2, 7, 6,9, GRAPHICS_INVERSE );
+      
+      dmd.drawCircle( 13, 8, 3, GRAPHICS_NORMAL );
+      dmd.drawCircle( 22, 8, 3, GRAPHICS_NORMAL );
+      dmd.drawCircle( 31, 8, 3, GRAPHICS_NORMAL );
+      dmd.drawCircle( 40, 8, 3, GRAPHICS_NORMAL );
+      
+      dmd.drawString(  47, 1 , segundo_reg, 2, GRAPHICS_NORMAL );
    }
+   
    else if (segundo==57){
-     if (bandera_limpiar==0){
-      dmd.clearScreen( true );
-      bandera_limpiar=1;
-    } 
-     dmd.drawCircle( 4, 8, 3, GRAPHICS_INVERSE );
+      if ( bandera_limpiar == 0){
+         dmd.clearScreen( true );
+         bandera_limpiar=1;
+      } 
+      dmd.drawCircle( 4, 8, 3, GRAPHICS_INVERSE );
+      
+      dmd.drawCircle( 13, 8, 3, GRAPHICS_INVERSE );
+      dmd.drawFilledBox( 12, 6, 14,10, GRAPHICS_INVERSE );
+      dmd.drawFilledBox( 11, 7, 15,9, GRAPHICS_INVERSE );
      
-     dmd.drawCircle( 13, 8, 3, GRAPHICS_INVERSE );
-     dmd.drawFilledBox( 12, 6, 14,10, GRAPHICS_INVERSE );
-     dmd.drawFilledBox( 11, 7, 15,9, GRAPHICS_INVERSE );
-    
-     dmd.drawCircle( 22, 8, 3, GRAPHICS_NORMAL );
-     dmd.drawCircle( 31, 8, 3, GRAPHICS_NORMAL );
-     dmd.drawCircle( 40, 8, 3, GRAPHICS_NORMAL );
-    
-     dmd.drawString(  47, 1 , segundo_reg, 2, GRAPHICS_NORMAL );
+      dmd.drawCircle( 22, 8, 3, GRAPHICS_NORMAL );
+      dmd.drawCircle( 31, 8, 3, GRAPHICS_NORMAL );
+      dmd.drawCircle( 40, 8, 3, GRAPHICS_NORMAL );
+     
+      dmd.drawString(  47, 1 , segundo_reg, 2, GRAPHICS_NORMAL );
    }
+
    else if (segundo==58){
-     if (bandera_limpiar==0){
-       dmd.clearScreen( true );
-       bandera_limpiar=1;
-     } 
-     dmd.drawCircle( 4, 8, 3, GRAPHICS_INVERSE );
+      if (bandera_limpiar==0){
+         dmd.clearScreen( true );
+         bandera_limpiar=1;
+      } 
+
+      dmd.drawCircle( 4, 8, 3, GRAPHICS_INVERSE );
+      
+      dmd.drawCircle( 13, 8, 3, GRAPHICS_INVERSE );
+      
+      dmd.drawCircle( 22, 8, 3, GRAPHICS_INVERSE );
+      dmd.drawFilledBox( 21, 6, 23,10, GRAPHICS_INVERSE );
+      dmd.drawFilledBox( 20, 7, 24,9, GRAPHICS_INVERSE );
+      
+      dmd.drawCircle( 31, 8, 3, GRAPHICS_NORMAL );
+      dmd.drawCircle( 40, 8, 3, GRAPHICS_NORMAL );
      
-     dmd.drawCircle( 13, 8, 3, GRAPHICS_INVERSE );
-     
-     dmd.drawCircle( 22, 8, 3, GRAPHICS_INVERSE );
-     dmd.drawFilledBox( 21, 6, 23,10, GRAPHICS_INVERSE );
-     dmd.drawFilledBox( 20, 7, 24,9, GRAPHICS_INVERSE );
-     
-     dmd.drawCircle( 31, 8, 3, GRAPHICS_NORMAL );
-     dmd.drawCircle( 40, 8, 3, GRAPHICS_NORMAL );
-    
-     dmd.drawString(  47, 1 , segundo_reg, 2, GRAPHICS_NORMAL );
+      dmd.drawString(  47, 1 , segundo_reg, 2, GRAPHICS_NORMAL );
    }
+   
    else if (segundo==59){
-     if (bandera_limpiar==0){
-       dmd.clearScreen( true );
-       bandera_limpiar=1;
-     } 
-     dmd.drawCircle( 4, 8, 3, GRAPHICS_INVERSE );
-     dmd.drawCircle( 13, 8, 3, GRAPHICS_INVERSE );
-     dmd.drawCircle( 22, 8, 3, GRAPHICS_INVERSE );
-     dmd.drawCircle( 31, 8, 3, GRAPHICS_INVERSE );
-     dmd.drawFilledBox( 30, 6, 32,10, GRAPHICS_INVERSE );
-     dmd.drawFilledBox( 29, 7, 33,9, GRAPHICS_INVERSE );
-     
-     dmd.drawCircle( 40, 8, 3, GRAPHICS_NORMAL );
-     dmd.drawLine( 54, 0, 54, 16,GRAPHICS_INVERSE );
-     
-     dmd.drawString(  47, 1 , segundo_reg, 2, GRAPHICS_NORMAL );
+      if (bandera_limpiar==0){
+         dmd.clearScreen( true );
+         bandera_limpiar=1;
+      } 
+      dmd.drawCircle( 4, 8, 3, GRAPHICS_INVERSE );
+      dmd.drawCircle( 13, 8, 3, GRAPHICS_INVERSE );
+      dmd.drawCircle( 22, 8, 3, GRAPHICS_INVERSE );
+      dmd.drawCircle( 31, 8, 3, GRAPHICS_INVERSE );
+      dmd.drawFilledBox( 30, 6, 32,10, GRAPHICS_INVERSE );
+      dmd.drawFilledBox( 29, 7, 33,9, GRAPHICS_INVERSE );
+      
+      dmd.drawCircle( 40, 8, 3, GRAPHICS_NORMAL );
+      dmd.drawLine( 54, 0, 54, 16,GRAPHICS_INVERSE );
+      
+      dmd.drawString(  47, 1 , segundo_reg, 2, GRAPHICS_NORMAL );
    }
+
    else if (segundo==0){
+
       dmd.drawFilledBox(0, 0, 64, 16, GRAPHICS_INVERSE );
       bandera_limpiar=0;
    }
